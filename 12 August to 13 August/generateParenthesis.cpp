935fc8a5bf423c60dd66b49abf5819a80fcaabf0
WiseCoder24 <anikethk249@gmail.com>
2022-08-13 18:54:43 +0530

    Updated directories

---
+++ 12 August to 13 August/generateParenthesis.cpp
@@ -0,0 +1,24 @@
+/*
+22. Generate Parenthesis
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+*/
+class Solution {
+public:
+    vector<string> ans;
+    void recurse(string s, int open, int close, int n){
+        if(open==n && close==n){
+            ans.push_back(s);
+            return;
+        }
+        if(open<n){
+            recurse(s+'(',open+1,close,n);
+        }
+        if(close<open){
+            recurse(s+')',open,close+1,n);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        recurse("",0,0,n);
+        return ans;
+    }
+};
